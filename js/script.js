089cfc2e8e2034322ce378f7d4a3658062df2a7e
Jordan Fitzgerald <jordan@jordanfitz.com>
2016-06-02 12:16:09 -0600

    Themes are now only loaded on pages that require them, updated version

--- js/script.js
+++ js/script.js
@@ -2,49 +2,52 @@
  * Loads the selected theme's CSS, along with any custom
  * CSS applied in the options.
  */
-chrome.storage.sync.get({
-	theme: "default.css",
-	customCSS: ""
-}, function(items) {
-	function listener() {
-		var style = document.createElement("style");
+function loadTheme() {
+	chrome.storage.sync.get({
+		theme: "default.css",
+		customCSS: ""
+	}, function(items) {
+		function listener() {
+			var style = document.createElement("style");
+			style.type = "text/css";
 
-		style.setAttribute("type", "text/css");
+			if (style.styleSheet) {
+				style.styleSheet.cssText = this.responseText;
+			} else {
+				style.appendChild(document.createTextNode(this.responseText));
+			}
 
-		if (style.styleSheet) {
-			style.styleSheet.cssText = this.responseText;
-		} else {
-			style.appendChild(document.createTextNode(this.responseText));
-		}
+			document.head.appendChild(style);
 
-		document.getElementsByTagName("head")[0].appendChild(style);
+			style = document.createElement("style");
+			style.type = "text/css";
 
-		style = document.createElement("style");
-		style.setAttribute("type", "text/css");
+			if (style.styleSheet) {
+				style.styleSheet.cssText = items.customCSS;
+			} else {
+				style.appendChild(document.createTextNode(items.customCSS));
+			}
 
-		if (style.styleSheet) {
-			style.styleSheet.cssText = items.customCSS;
-		} else {
-			style.appendChild(document.createTextNode(items.customCSS));
-		}
+			document.head.appendChild(style);
+		};
 
-		document.getElementsByTagName("head")[0].appendChild(style);
-	};
-
-	var request = new XMLHttpRequest();
-	
-	request.addEventListener("load", listener);
-	request.open("GET", chrome.extension.getURL("themes/" + items.theme));
-	request.send();
-});
+		var request = new XMLHttpRequest();
+		
+		request.addEventListener("load", listener);
+		request.open("GET", chrome.extension.getURL("themes/" + items.theme));
+		request.send();
+	});
+}
 
 function init() {
 	/*
 	 * Code adapted from JSONView.
 	 * https://chrome.google.com/webstore/detail/jsonview/chklaanhfefbnpoihckbnefhakgolnmc
 	 */
-	 
+
 	if(document.body && (document.body.children.length === 1 && document.body.children[0].tagName === "PRE")) {
+		loadTheme();
+
 		var pre = document.body.children[0];
 		var content = pre.innerHTML;
 
